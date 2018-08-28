e7cc6b479b33d273b3dffa0cea833fbde58eba44
Lexden <schendel21@up.edu>
2018-08-28 10:30:16 -0700

    Initial commit!

---
+++ app/src/main/java/com/example/schendel21/app1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.schendel21.app1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void toggleText(View v){
+        textView.setText("You clicked the button!");
+    }
+}
